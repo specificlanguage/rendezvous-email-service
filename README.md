9f07a089b096d33cfb1aceed7ff167f6d6f94875
specificlanguage <specificlanguage@gmail.com>
2023-10-30 22:58:07 -0400

    feat: Email sending (pt 1)
    - An attempt to send emails via react-email/sendgrid
    - This unfortunately does not work due to the fact that React does not exist on the Lambda which makes it impossible to use, looking into ways to get it there

--- README.md
+++ README.md
@@ -65,7 +65,7 @@ The project code base is mainly located within the `src` folder. This folder is 
 ├── src
 │   ├── functions               # Lambda configuration and source code folder
 │   │   ├── hello
-│   │   │   ├── handler.ts      # `Hello` lambda source code
+│   │   │   ├── handler.tsx      # `Hello` lambda source code
 │   │   │   ├── index.ts        # `Hello` lambda Serverless configuration
 │   │   │   ├── mock.json       # `Hello` lambda input parameter, if any, for local invocation
 │   │   │   └── schema.ts       # `Hello` lambda input event JSON-Schema
--- serverless.ts
+++ serverless.ts
@@ -1,35 +1,32 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import invite from "@functions/invite";
 
 const serverlessConfiguration: AWS = {
-  service: 'rendezvous-email-service',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "rendezvous-email-service",
+  useDotenv: true,
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs18.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-    },
   },
   // import the function via paths
-  functions: { hello },
+  functions: { invite },
   package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
